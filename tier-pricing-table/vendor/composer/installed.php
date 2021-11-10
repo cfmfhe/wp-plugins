52b1963012e376f6bce6253649293f9dc5873bf5
Andrew Newland <anewland@gmail.com>
2021-11-10 10:39:40 -0600

    Added additional Woocommerce Plugins

---
+++ tier-pricing-table/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '7903461e4b2288373c492cbfccf42c293dfa8bab',
+    'name' => 'u2code/tiered-pricing-table',
+  ),
+  'versions' => 
+  array (
+    'u2code/tiered-pricing-table' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '7903461e4b2288373c492cbfccf42c293dfa8bab',
+    ),
+  ),
+);
